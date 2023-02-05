317b076dd5fe41514b2296b33b50f6bd8a9560ff
Jordan Benrezkallah <jordan.benrezkallah@student.umons.ac.be>
2023-02-05 20:27:48 +0100

    ajout de source() dans certains scripts

--- Script/start.R
+++ Script/start.R
@@ -2,44 +2,79 @@
 library(dplyr)
 library(tidyr) # fonction gather
 library(vegan) # specpool () estimateR() poolaccum() estaccumR()
+library(iNEXT)
 library(ggplot2)
 library("readxl") 
 library(forcats)
+library(RODBC)
 library(viridis) # couleur daltonien
 #library(grDevices) # pdf() dev.off()
+
+
+
+
+# Créer une connexion à la base de données Access
+# Pour la configuration du fichier .mdb voir README_IMPORT_DB.Rmd
+con <- odbcConnect("Memoire_Jordan")
+
+SpecCondStat <- sqlQuery(con, "
+SELECT
+  PROJECT.CODE,
+  SPEC.*,
+  COND.*,
+  STAT.*
+FROM
+  PROJECT
+INNER JOIN
+  (
+    STAT
+    INNER JOIN
+      (
+        COND
+        INNER JOIN SPEC ON COND.COND_ID = SPEC.COND_ID
+      ) ON STAT.STAT_ID = COND.STAT_ID
+  ) ON PROJECT.PROJECT_ID = STAT.PROJECT_ID
+")
+
+
+# Fermer la connexion à la base de données
+odbcClose(con)
 
 
 # Import
 # Tour PC
 #setwd("D:/OneDrive - UMONS/UMONS/MA2/MEMOIRE/Statistique/R/STAT-MEMOIRE/RAW")
 # Laptop
-setwd("C:/Users/Jordan/OneDrive - UMONS/UMONS/MA2/MEMOIRE/Statistique/R/STAT-MEMOIRE/RAW/")
+# setwd("C:/Users/Jordan/OneDrive - UMONS/UMONS/MA2/MEMOIRE/Statistique/R/STAT-MEMOIRE/RAW")
+# 
+# # Import
+# SCS <- read_excel("C:/Users/Jordan/OneDrive - UMONS/UMONS/MA2/MEMOIRE/Statistique/R/STAT-MEMOIRE/RAW/Traits abeilles")
+
+SCS <- SpecCondStat
 
 
-library(RODBC)
-my_database <- RODBC::odbcConnectAccess("C:/Users/Jordan/OneDrive - UMONS/UMONS/MA2/MEMOIRE/DFF/DATA/Clean/Mémoire_2022(clean).mdb")
-
-RODBC::sqlTables(my_database)
-scs <- RODBC::sqlFetch(channel.sadc,"SADCQ")
-
-
-# Import
-SCS <- read_excel("SpecCondStat.xls")
+# # Renommer plus simplement
+# rename(SCS, "sp" = "SPEC.TAXPRIO" ) -> SCS
+# rename(SCS, "genus" = "SPEC.GEN" ) -> SCS
+# rename(SCS, "plante" = "COND.TAXPRIO" ) -> SCS
+# rename(SCS, "pl_fam" = "COND.GR2" ) -> SCS
+# rename(SCS, "family" = "SPEC.GR2" ) -> SCS
+# rename(SCS, "site" = "TOPO" ) -> SCS
 
 # Renommer plus simplement
-rename(SCS, "sp" = "SPEC.TAXPRIO" ) -> SCS
-rename(SCS, "genus" = "SPEC.GEN" ) -> SCS
-rename(SCS, "plante" = "COND.TAXPRIO" ) -> SCS
-rename(SCS, "pl_fam" = "COND.GR2" ) -> SCS
-rename(SCS, "familly" = "SPEC.GR2" ) -> SCS
+rename(SCS, "sp" = "TAXPRIO" ) -> SCS
+rename(SCS, "genus" = "GEN" ) -> SCS
+rename(SCS, "plante" = "TAXPRIO.1" ) -> SCS
+rename(SCS, "pl_fam" = "GR2.1" ) -> SCS
+rename(SCS, "family" = "GR2" ) -> SCS
 rename(SCS, "site" = "TOPO" ) -> SCS
+rename(SCS, "projet" = "CODE" ) -> SCS
 
 # Remplacement des noms d'espèces désuets
 SCS$sp[SCS$sp == "Bombus (Bombus)  sp."] <- "Terrestribombus  sp."
 SCS$sp[SCS$sp == "Bombus lucorum"] <- "Terrestribombus  sp."
 SCS$sp[SCS$sp == "Bombus terrestris"] <- "Terrestribombus  sp."
 SCS$sp[SCS$sp == "Chalicodoma ericetorum"] <- "Megachile ericetorum"
-SCS$sp[SCS$sp == "Halictus tumulorum"] <- "Seladonia tumulorum"
 SCS$sp[SCS$sp == "Halictus tumulorum"] <- "Seladonia tumulorum"
 
 # Retirer les observations contenant l'espèce : "Apis mellifera"
@@ -50,3 +85,4 @@ SCS$site[SCS$site == "Les Gourmandes de la Procession"] <- "Gourmandes"
 
 # Retirer le site de Condorcet
 SCS <- filter(SCS, site != "Condorcet" )
+
